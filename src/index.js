afec950bb12c0e51c1b04fa247f7399ce06d51a7
Orion Hall <orionmhall@gmail.com>
2021-12-22 22:29:30 -0500

    Rename app.jsx -> App.jsx

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './MountainClimber/app';
+import App from './MountainClimber/App';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
